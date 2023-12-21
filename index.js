d78b5b92ce68000145db32133f118d9b92497c20
benjamin gutierrez <benjamingutiterrez89@gmail.com>
2023-12-21 10:37:08 +0100

    agreando dependencia html linter

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let numero1 = '30'
-numero1 = 6
-console.log(numero1)
+const numero1 = 30
+const numero2=2
 
-// adsñkljadsf j ñasjdfñajds kfñaslkj añlkjf añlskdfjñlkdfj añlsdjfk aslñkdjf asñlkfj asñdlfasdfkjl asdfklñj adfñlkj asñdlfjkasdfjñlk asdfñjkl dfsj
+const suma = numero1 + numero2
+console.log('la suma de los dos numeros es:')
+console.log(suma)
