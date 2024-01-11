2cd75b906fdb333bc57ef0caf51c988b58f21710
benjamin gutierrez <benjamingutiterrez89@gmail.com>
2024-01-11 14:28:31 +0100

    termiando de hacer ejercios de jugo y sumar

---
+++ jugo.js
@@ -0,0 +1,15 @@
+/* progrando con funciones felchas */
+
+const piezasManzanas = (numManzana) => 4 * numManzana
+const piezasPera = (numPeras) => 3 * numPeras
+
+const hacerZUmo = (numManzana, numPeras) => {
+    const numeropiezasManzana = piezasManzanas(numManzana)
+    const numeropiezasPera = piezasPera(numPeras)
+    const totaldepiezas = numeropiezasManzana + numeropiezasPera
+    return totaldepiezas
+}
+
+
+
+console.log(`haz utilizado ${hacerZUmo(3, 4)}`)
---
+++ sumar.js
@@ -0,0 +1,20 @@
+// FORMA TRADICIONAL
+// function sumar(num1, num2) {
+//   return num1 + num2
+// }
+
+/* guardonado la funcion en un a variable*/
+/*const sumar = function (num1,num2)
+{
+    return num1+num2
+} */
+
+// FUNCIONES FLECHA forma larga
+// const sumar = (num1, num2) => {
+//   return num1 + num2
+// }
+
+// FUNCIONES FLECHA forma corta
+const sumar = (num1, num2) => num1 + num2
+
+console.log(`1+2 = ${sumar(1, 2)}`)
